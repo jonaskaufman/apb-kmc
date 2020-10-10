d8f1b43d61e82d864e8a1ec5dc1496df68d8e85f
Jonas Kaufman <jonas.l.kaufman@gmail.com>
2020-10-09 17:32:12 -0700

    Wrapper implementation

--- wrapper.cpp
+++ wrapper.cpp
@@ -1,3 +1,114 @@
 #include "wrapper.hpp"
+#include <cmath>
+#include <vector>
 
+std::vector<int> spacings_for_sinusoidal_composition(const BOUNDARY_TYPE& boundary_type,
+                                                     const double& composition_average,
+                                                     const double& composition_amplitude,
+                                                     const int& target_height)
+{
+    std::vector<int> spacing_samples(target_height, 0);
+    for (int y = 0; y < target_height; y++)
+    {
+        double composition = composition_amplitude * std::cos(y * 2 * M_PI / target_height) + composition_average;
+        int spacing = 0;
+        if (boundary_type == BOUNDARY_TYPE::MINUS)
+        {
+            spacing = std::round(composition / (1 - 2 * composition));
+        }
+        else if (boundary_type == BOUNDARY_TYPE::PLUS)
+        {
+            spacing = std::round(composition / (2 * composition - 1));
+        }
+        spacing_samples[y] = spacing;
+    }
+    std::vector<int> spacings;
+    int y = 0;
+    while (y < target_height / 2)
+    {
+        int next_spacing = spacing_samples[y];
+        spacings.push_back(next_spacing);
+        y += next_spacing;
+    }
+    int length = spacings.size();
+    for (int i = 0; i < length; i++)
+    {
+        spacings.push_back(spacings[length - 1 - i]);
+    }
+    return spacings;
+}
 
+SimulationWrapper::SimulationWrapper(const BOUNDARY_TYPE& boundary_type,
+                                     const int& width,
+                                     const std::vector<int>& initial_spacings,
+                                     const double& temperature)
+    : boundary_type{boundary_type}, width{width}, initial_spacings{initial_spacings}, temperature{temperature}
+{
+}
+
+Simulation SimulationWrapper::setup()
+{
+    SimulationCellGrid grid(width, initial_spacings, stagger_grid());
+    Simulation simulation(boundary_type, grid, temperature);
+    return simulation;
+}
+
+void SimulationWrapper::perform_single(const int& total_steps,
+                                       const int& print_interval,
+                                       std::ofstream& output_file_stream)
+{
+    Simulation simulation = setup();
+    for (int n = 0; n < total_steps; n++)
+    {
+        if (n % print_interval == 0)
+        {
+            output_file_stream << simulation.get_time() << std::endl;
+            simulation.print_phase_pixel_grid(output_file_stream);
+        }
+        simulation.step();
+    }
+}
+
+void SimulationWrapper::perform_set(const int& total_simulations,
+                                    const int& total_steps,
+                                    const int& print_interval,
+                                    std::ofstream& output_file_stream)
+{
+    std::vector<double> times;
+    std::vector<std::vector<std::vector<double>>> average_spacings;
+    for (int k = 0; k < total_simulations; k++)
+    {
+        std::cout << "Running simulation " << k << "...";
+        average_spacings.emplace_back(std::vector<std::vector<double>>());
+        Simulation simulation = setup();
+        for (int n = 0; n < total_steps; n++)
+        {
+            if (n % print_interval == 0)
+            {
+                if (k == 0)
+                {
+                    times.emplace_back(simulation.get_time());
+                }
+                average_spacings[k].emplace_back(simulation.get_horizontal_pixel_average_spacings());
+            }
+            simulation.step();
+        }
+        std::cout << "done." << std::endl;
+    }
+    std::cout << "Writing results file...";
+    for (int t = 0; t < times.size(); t++)
+    {
+        output_file_stream << times[t] << std::endl;
+        for (int k = 0; k < total_simulations; k++)
+        {
+            for (auto& a : average_spacings[k][t])
+            {
+                output_file_stream << a << " ";
+            }
+            output_file_stream << "\n";
+        }
+    }
+    std::cout << "done." << std::endl;
+}
+
+bool SimulationWrapper::stagger_grid() const { return (boundary_type == BOUNDARY_TYPE::MINUS); }
--- wrapper.hpp
+++ wrapper.hpp
@@ -16,26 +16,36 @@ class SimulationWrapper
 public:
     SimulationWrapper() = delete;
 
-    /// 
+    ///
     SimulationWrapper(const BOUNDARY_TYPE& boundary_type,
+                      const int& width,
                       const std::vector<int>& initial_spacings,
                       const double& temperature);
 
+    /// Set up a single simulation
+    Simulation setup();
+
     /// Perform a single simulation
-    void perform_single(const int& total_steps, const int& print_interval, const std::ofstream& output_file_stream);
+    void perform_single(const int& total_steps, const int& print_interval, std::ofstream& output_file_stream);
 
     /// Perform a set of simulations under the same conditions
-    void perform_set(const int& total_steps, const int& print_interval, const std::ofstream& output_file_stream);
+    void perform_set(const int& total_simulations, const int& total_steps, const int& print_interval, std::ofstream& output_file_stream);
 
 private:
     /// Boundary type for simulations
     const BOUNDARY_TYPE boundary_type;
+
+    /// Width of simulation grid
+    const int width;
 
     /// Initial boundary spacings for simulations
     const std::vector<int> initial_spacings;
 
     /// Temperature for simulations
     const double temperature;
+
+    /// Determine whether grid should be staggered based on boundary_type
+    bool stagger_grid() const;
 };
 
 #endif

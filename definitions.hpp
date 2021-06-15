389377b5dbeab030f30859d143308de4af06ada6
Jonas Kaufman <jonas.l.kaufman@gmail.com>
2021-06-14 17:13:07 -0700

    Began simulation refactor

--- definitions.hpp
+++ definitions.hpp
@@ -4,8 +4,12 @@
 #include <utility>
 #include <vector>
 
+// TODO: Move other aliases here?
+
 // Kinetic event represented by indices of grid cells to flip
 using Event = std::vector<std::pair<int, int>>;
+
+using ID = int;
 
 /// Antiphase boundary types (zeta minus or zeta plus)
 enum class BOUNDARY_TYPE
--- simulation.hpp
+++ simulation.hpp
@@ -6,33 +6,10 @@
 #include "grid.hpp"
 #include <fstream>
 #include <iostream>
+#include <lotto/rejection.hpp>
+#include <lotto/rejection_free.hpp>
 #include <random>
 #include <vector>
-
-/**
- * Random number generator
- *
- * Allows sampling of random integers and reals.
- */
-class RandomGenerator
-{
-public:
-    /// Default constructor, automatically seeds generator
-    RandomGenerator();
-
-    /// Returns a random integer between 0 and maximum_value (inclusive)
-    int sample_integer_range(int maximum_value);
-
-    /// Returns a random real from the unit interval
-    double sample_unit_interval();
-
-private:
-    /// 64-bit Mersenne Twister generator
-    std::mt19937_64 generator;
-
-    /// Uniform real distribution on unit interval
-    std::uniform_real_distribution<double> uniform_unit_interval_distribution;
-};
 
 /**
  * Kinetic Monte Carlo simulation
@@ -51,6 +28,11 @@ public:
     /// Runs a number of steps equal to the number of events considered
     void pass();
 
+    /// Returns the simulation time
+    double get_time() const { return time; }
+
+    // TODO: Check dimensions of grid outputs
+    //
     /// Returns a pixel grid of phase values (0 or 1). Width is doubled for zeta minus simulation.
     PixelGrid get_phase_pixel_grid() const;
 
@@ -60,12 +42,16 @@ public:
     /// Returns average composition profile obtained by averaging each row of the composition pixel grid.
     std::vector<double> get_average_composition_profile() const;
 
-    /// Returns the simulation time
-    double get_time() const { return time; }
-
 private:
     /// Simulation cell grid to store phase values
-    SimulationCellGrid grid;
+    std::shared_ptr<SimulationCellGrid> grid_ptr;
+
+    /// Rate calculator
+    std::shared_ptr<EventRateCalculator> rate_calculator_ptr;
+
+    /// Event selector
+    // TODO: Change to rejection-free eventually
+    std::unique_ptr<lotto::RejectionEventSelector<ID, EventRateCalculator>> selector_ptr;
 
     /// Type of boundary being simulated (true for minus, false for plus)
     const bool boundary_minus;
@@ -77,53 +63,16 @@ private:
     const double temperature;
 
     /// List of all possible events that can occur over the course of a simulation
-    std::vector<Event> event_list;
-
-    /// Cache to store rates (to avoid recaculation)
-    std::vector<double> rate_cache;
-
-    /// Random number generator to be used throughout simulation
-    RandomGenerator random_generator;
-
-    ///
-    EventRateCalculator rate_calculator;
-
-    /// Populates the event list based on the grid dimensions and boundary type
-    // NOTE: for two atom events, they are always listed in L->R order
-    void populate_event_list();
-
-    ///
-    void reset_rate_cache();
+    const std::vector<Event> event_list;
 
     /// Returns the rate of an event given the current configuration
     double calculate_rate(const Event& event) const;
 
-    ///
-    bool is_valid_event(const Event& event) const;
+    /// Generate event list
+    std::vector<Event> generate_event_list() const;
 
-    ///
-    bool is_valid_event_zeta_minus(const Event& event) const;
-
-    ///
-    bool is_valid_event_zeta_plus(const Event& event) const;
-
-    /// Returns the kinetic barrier for an event
-    double calculate_barrier(const Event& event) const;
-
-    ///
-    double calculate_barrier_zeta_minus(const Event& event) const;
-
-    ///
-    double calculate_barrier_zeta_plus(const Event& event) const;
-
-    /// Returns the sublattice (A or B) of the atom in given cell based on boundary type and phase
-    SUBLATTICE get_sublattice_of_cell(int x, int y);
-
-    /// Returns the total boundary repulsion energy change for an event
-    double calculate_total_repulsion_energy_change(const Event& event) const;
-
-    /// Returns the boundary repulsion energy change for a single cell flip
-    double calculate_repulsion_energy_change(int x, int y) const;
+    // Generate impact table
+    std::map<ID, Event> generate_impact_table() const;
 };
 
 #endif
